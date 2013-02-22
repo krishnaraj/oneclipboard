1d08be3c199833930d725dd306536a27d5307b60
krishnaraj <krisgenre@gmail.com>
2013-02-23 00:44:22 +0530

    Various changes

--- oneclipboarddesktop/src/com/cb/oneclipboard/desktop/Client.java
+++ oneclipboarddesktop/src/com/cb/oneclipboard/desktop/Client.java
@@ -1,64 +1,59 @@
 package com.cb.oneclipboard.desktop;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import com.cb.oneclipboard.lib.ApplicationProperties;
+import com.cb.oneclipboard.lib.Callback;
+import com.cb.oneclipboard.lib.DefaultPropertyLoader;
+import com.cb.oneclipboard.lib.Message;
+import com.cb.oneclipboard.lib.MessageListener;
 import com.cb.oneclipboard.lib.MessageSender;
+import com.cb.oneclipboard.lib.socket.ClipboardConnector;
 
 public class Client {
 
-	private static ServerSocket serverSocket;
-	private static Socket clientSocket;
-	private static InputStreamReader inputStreamReader;
-	private static BufferedReader bufferedReader;
-	private static String message;
+	private static String serverAddress = null;
+	private static int port;
+	private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+	public static final String[] PROP_LIST = { "config.properties" };
 
 	public static void main(String[] args) {
-		register();
-
-		try {
-			serverSocket = new ServerSocket(5555); // Server socket
-
-		} catch (IOException e) {
-			System.out.println("Could not listen on port: 5555");
-		}
-
-		System.out.println("Server started. Listening on port 5555");
-
-		while (true) {
-			try {
-
-				clientSocket = serverSocket.accept(); // accept the client connection
-				inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-				bufferedReader = new BufferedReader(inputStreamReader); // get the client message
-				message = bufferedReader.readLine();
-
-				System.out.println("Received: " + message);
-				inputStreamReader.close();
-				clientSocket.close();
-				
-				TextTransfer textTransfer = new TextTransfer();
-				textTransfer.setClipboardContents(message);
-				
-			} catch (IOException ex) {
-				System.out.println("Problem in message reading");
-			}
-		}
-
-	}
-	
-	private static void register() {
-		try{
-			MessageSender.register(
-					ApplicationProperties.getStringProperty("server"), 
-					ApplicationProperties.getIntProperty("port"));
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		final TextTransfer textTransfer = new TextTransfer();
 		
+		// Initialize properties
+		ApplicationProperties.loadProperties(PROP_LIST, new DefaultPropertyLoader());
+		serverAddress = ApplicationProperties.getStringProperty("server");
+		port = ApplicationProperties.getIntProperty("port");
+		
+		// Send a register message to the server so that the client ip can be registered
+		MessageSender.sendRegisterMessage(serverAddress, port);
+		
+		// Poll the clipboard for changes
+		final ClipboardPollTask clipboardPollTask = new ClipboardPollTask(textTransfer, new Callback() {
+
+			@Override
+			public void execute(Object object) {
+				String clipboardText = (String) object;
+				MessageSender.send(serverAddress, port, clipboardText);
+			}
+		});
+		
+		// Listen for clipboard content from other clients
+		ClipboardConnector.startListening(ApplicationProperties.getIntProperty("port"), new MessageListener() {
+
+			@Override
+			public void onMessageReceived(String ip, Message message) {
+				clipboardPollTask.updateClipboardContent(message.getText());
+				textTransfer.setClipboardContents(message.getText());
+			}
+		});
+		
+		// Run the poll task every 2 seconds
+		final ScheduledFuture<?> pollHandle = scheduler.scheduleAtFixedRate(clipboardPollTask, 0, 2, TimeUnit.SECONDS);
+
 	}
 }

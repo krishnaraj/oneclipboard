597022c801da15b8f1b31cbe3a3454d45f529126
krishnaraj <krisgenre@gmail.com>
2013-02-23 00:47:24 +0530

    Various changes

--- oneclipboardserver/src/com/cb/oneclipboard/server/Server.java
+++ oneclipboardserver/src/com/cb/oneclipboard/server/Server.java
@@ -1,63 +1,40 @@
 package com.cb.oneclipboard.server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import com.cb.oneclipboard.lib.ApplicationProperties;
 import com.cb.oneclipboard.lib.DefaultPropertyLoader;
 import com.cb.oneclipboard.lib.Message;
+import com.cb.oneclipboard.lib.MessageListener;
 import com.cb.oneclipboard.lib.MessageSender;
+import com.cb.oneclipboard.lib.socket.ClipboardConnector;
 
 public class Server {
-	
-	public static final String[] PROP_LIST = {"config.properties"};
 
-	private static ServerSocket serverSocket;
-	private static Socket clientSocket;
-	private static InputStream inputStream;
-	private static ObjectInputStream objInputStream;
-	private static Message message;
+	public static final String[] PROP_LIST = { "config.properties" };
 
 	public static void main(String[] args) {
 		ApplicationProperties.loadProperties(PROP_LIST, new DefaultPropertyLoader());
 
-		try {
-			serverSocket = new ServerSocket(4444); // Server socket
+		ClipboardConnector.startListening(4444, new MessageListener() {
 
-		} catch (IOException e) {
-			System.out.println("Could not listen on port: 4444");
-		}
+			@Override
+			public void onMessageReceived(String ip, Message message) {
+				switch (message.getMessageType()) {
+				case REGISTER: Registery.register(ip); break;
+				case TEXT: broadcastMessage(ip, message); break;
+				default: System.out.println("Unknown message type: " + message.getMessageType());
+				}
 
-		System.out.println("Server started. Listening on port 4444");
+			}
+		});
 
-		while (true) {
-			try {
-
-				clientSocket = serverSocket.accept(); // accept the client connection
-				inputStream = clientSocket.getInputStream();
-				objInputStream = new ObjectInputStream(inputStream); // get the client message
-				message = (Message)objInputStream.readObject();
-
-				System.out.println(message);
-				inputStream.close();
-				clientSocket.close();
-				
-				String hostAddress = clientSocket.getInetAddress().getHostAddress();
-				System.out.println("hostAddress: " + hostAddress);
-				
-				MessageSender.send(
-						ApplicationProperties.getStringProperty("server"), 
-						ApplicationProperties.getIntProperty("port"),
-						message);
-
-			} catch (Exception ex) {
-				System.out.println("Problem in message reading");
+	}
+	
+	private static void broadcastMessage(String hostAddress, Message message) {
+		for (String ip : Registery.getClients()){
+			if(!hostAddress.equals(ip)){
+				MessageSender.send(ip, 4444, message);
 			}
 		}
-
+		
 	}
 }

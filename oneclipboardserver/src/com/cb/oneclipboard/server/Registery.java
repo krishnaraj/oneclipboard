e6fbfdaabffc17a11a65b7b71e1190c9e5ff1315
krishnaraj <krisgenre@gmail.com>
2013-02-23 21:52:11 +0530

    Randomizing client port and other changes

--- oneclipboardserver/src/com/cb/oneclipboard/server/Registery.java
+++ oneclipboardserver/src/com/cb/oneclipboard/server/Registery.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Registery {
-	private static List<String> clients = new ArrayList<>();
+	private static List<ClientData> clients = new ArrayList<>();
 	
-	public static void register(String ip){
-		clients.add(ip);
+	public static void register(String ip, int port){
+		System.out.println("Registering " + ip + ":" + port);
+		clients.add(new ClientData(ip, port));
 	}
 	
-	public static List<String> getClients(){
+	public static List<ClientData> getClients(){
 		return clients;
 	}
 }
--- oneclipboardserver/src/com/cb/oneclipboard/server/Server.java
+++ oneclipboardserver/src/com/cb/oneclipboard/server/Server.java
@@ -1,44 +1,64 @@
 package com.cb.oneclipboard.server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import com.cb.oneclipboard.lib.ApplicationProperties;
 import com.cb.oneclipboard.lib.DefaultPropertyLoader;
 import com.cb.oneclipboard.lib.Message;
-import com.cb.oneclipboard.lib.MessageListener;
 import com.cb.oneclipboard.lib.MessageSender;
+import com.cb.oneclipboard.lib.SocketListener;
 import com.cb.oneclipboard.lib.socket.ClipboardConnector;
 
 public class Server {
 
 	public static final String[] PROP_LIST = { "config.properties" };
 	private static int serverPort;
-	private static int clientPort;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		pipeSysoutToFile();
+		
 		ApplicationProperties.loadProperties(PROP_LIST, new DefaultPropertyLoader());
 		serverPort = ApplicationProperties.getIntProperty("server_port");
-		clientPort = ApplicationProperties.getIntProperty("client_port");
 
-		ClipboardConnector.startListening(serverPort, new MessageListener() {
+		ClipboardConnector.startListening(serverPort, new SocketListener() {
 
 			@Override
 			public void onMessageReceived(String ip, Message message) {
 				switch (message.getMessageType()) {
-				case REGISTER: Registery.register(ip); break;
+				case REGISTER: Registery.register(ip, message.getReplyPort()); break;
 				case TEXT: broadcastMessage(ip, message); break;
 				default: System.out.println("Unknown message type: " + message.getMessageType());
 				}
 
+			}
+
+			@Override
+			public void onPortReady(int replyPort) {
+				
 			}
 		});
 
 	}
 	
 	private static void broadcastMessage(String hostAddress, Message message) {
-		for (String ip : Registery.getClients()){
-			if(!hostAddress.equals(ip)){
-				MessageSender.send(ip, clientPort, message);
+		for (ClientData clientData : Registery.getClients()){
+			String source = hostAddress + ":" + message.getReplyPort();
+			String destination = clientData.getIp() + ":" + clientData.getPort();
+			if(!source.equals(destination)){
+				System.out.println("Broadcasting " + message.getText() + " to" + clientData.getIp() + ":" + clientData.getPort());
+				MessageSender.send(clientData.getIp(), clientData.getPort(), message);
 			}
 		}
 		
+	}
+	
+	private static void pipeSysoutToFile() throws FileNotFoundException{
+		File file  = new File("sysout.log");
+        PrintStream printStream = new PrintStream(new FileOutputStream(file));
+        System.setOut(printStream);
+        System.setErr(printStream);
 	}
 }

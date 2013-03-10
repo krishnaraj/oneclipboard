b11a0585dc84cf2f17650339680e564566ab1995
krishnaraj <krisgenre@gmail.com>
2013-03-10 12:58:17 +0530

    Admin Server should run continuously

--- oneclipboardserver/src/com/cb/oneclipboard/server/admin/AdminServer.java
+++ oneclipboardserver/src/com/cb/oneclipboard/server/admin/AdminServer.java
@@ -15,42 +15,40 @@ public class AdminServer {
 			
 			@Override
 			public void run() {
-
-				ServerSocket serverSocket = null;
-
-				try {
-					serverSocket = new ServerSocket(serverPort);
-					System.out.println("Admin Server started on port " + serverPort);
-				} catch (IOException e) {
-					System.err.println("Could not listen on port: " + serverPort);
-				}
-
-				Socket clientSocket = null;
-				try {
-					clientSocket = serverSocket.accept();
-				} catch (IOException e) {
-					System.err.println("Accept failed.");
-				}
-
-				try {
-					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-					BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-					String inputLine, outputLine;
-					AdminCommandProcessor commandProcessor = new AdminCommandProcessor();
-
-					while ((inputLine = in.readLine()) != null) {
-						outputLine = commandProcessor.processCommand(inputLine);
-						out.println(outputLine);
-						if (outputLine.equals("exit"))
-							break;
+				while (true) {
+					ServerSocket serverSocket = null;
+					try {
+						serverSocket = new ServerSocket(serverPort);
+						System.out.println("Admin Server started on port " + serverPort);
+					} catch (IOException e) {
+						System.err.println("Could not listen on port: " + serverPort);
 					}
+					Socket clientSocket = null;
+					try {
+						clientSocket = serverSocket.accept();
+					} catch (IOException e) {
+						System.err.println("Accept failed.");
+					}
+					try {
+						PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+						BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+						String inputLine, outputLine;
+						AdminCommandProcessor commandProcessor = new AdminCommandProcessor();
 
-					out.close();
-					in.close();
-					clientSocket.close();
-					serverSocket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
+						while ((inputLine = in.readLine()) != null) {
+							outputLine = commandProcessor.processCommand(inputLine);
+							out.println(outputLine);
+							if (outputLine.equals("exit"))
+								break;
+						}
+
+						out.close();
+						in.close();
+						clientSocket.close();
+						serverSocket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}, "Admin Server Thread");

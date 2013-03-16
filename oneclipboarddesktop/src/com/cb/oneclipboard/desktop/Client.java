3bff892b9582ac5e403c125e04f58efe18b72a6a
krishome <krishome@Vostro-3500.(none)>
2013-03-16 19:33:27 +0530

    Single instance + system tray

--- oneclipboarddesktop/src/com/cb/oneclipboard/desktop/Client.java
+++ oneclipboarddesktop/src/com/cb/oneclipboard/desktop/Client.java
@@ -2,11 +2,9 @@ package com.cb.oneclipboard.desktop;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -23,7 +21,7 @@ import com.cb.oneclipboard.lib.SocketListener;
 import com.cb.oneclipboard.lib.User;
 import com.cb.oneclipboard.lib.socket.ClipboardConnector;
 import com.jezhumble.javasysmon.JavaSysMon;
-import com.sun.org.apache.bcel.internal.generic.LCONST;
+import com.jezhumble.javasysmon.OsProcess;
 
 public class Client {
 
@@ -36,7 +34,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		final TextTransfer textTransfer = new TextTransfer();
-		
+
 		init(args);
 
 		// Poll the clipboard for changes
@@ -72,7 +70,13 @@ public class Client {
 
 	public static void init(String args[]) {
 		pipeSysoutToFile();
-		
+
+		try {
+			lock();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		// Initialize properties
 		ApplicationProperties.loadProperties(PROP_LIST, new DefaultPropertyLoader());
 		serverAddress = ApplicationProperties.getStringProperty("server");
@@ -84,31 +88,49 @@ public class Client {
 			} catch (Exception e) {
 			}
 		}
-		
+
 		Tray.init(args);
-		
+
 		// Set user
 		user = new User("testuser", "testpass");
 	}
-	
+
 	static String lockFileName = System.getProperty("user.home") + File.separator + "oneclipboard.lock";
-	
+	static JavaSysMon monitor = new JavaSysMon();
+
 	public static void lock() throws Exception {
-		JavaSysMon monitor =   new JavaSysMon();
-		int pid = monitor.currentPid();
-		
-		FileOutputStream fos = new FileOutputStream(lockFileName);
-		fos.write(String.valueOf(pid).getBytes());
-	}
-	
-	public static void isRunning(){
-		File lockFile = new File(lockFileName);
-		if(lockFile.exists()){
-			
+		int currentPid = monitor.currentPid();
+		if (isRunning()) {
+			System.err.println("Application already running");
+			System.exit(0);
+		} else {
+			// Write current PID to file.
+			File lockFile = new File(lockFileName);
+			lockFile.deleteOnExit();
+			FileOutputStream fos = new FileOutputStream(lockFile);
+			fos.write(String.valueOf(currentPid).getBytes());
+			fos.flush();
+			fos.close();
 		}
+
 	}
-	
-	private static int getPID(File lockFile){
+
+	public static boolean isRunning() {
+		File lockFile = new File(lockFileName);
+		boolean isRunning = false;
+		if (lockFile.exists()) {
+			int pid = getPID(lockFile);
+			if (pid > 0) {
+				OsProcess process = monitor.processTree().find(getPID(lockFile));
+				if (process != null && process.processInfo().getPid() == pid) {
+					isRunning = true;
+				}
+			}
+		}
+		return isRunning;
+	}
+
+	private static int getPID(File lockFile) {
 		int pid = -1;
 		// Read PID
 		BufferedReader reader = null;
@@ -117,7 +139,7 @@ public class Client {
 			pid = Integer.parseInt(reader.readLine().trim());
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally{
+		} finally {
 			try {
 				reader.close();
 			} catch (Exception e) {
